0dccfe44fb4ab9f61da24edd9f9dc2c252040c59
NihalKumar1 <164127594+NihalKumar1@users.noreply.github.com>
2025-04-16 14:18:47 -0400

    Add files via upload

---
+++ MinSurveyCode.m
@@ -0,0 +1,21 @@
+clc
+clear
+format compact
+Population=100;
+a=[1:1:Population]';
+c=[1:1:Population];
+d=0.69 #Percentage
+b=(1./c);
+n=a*b;
+m=(abs(d-n));
+x=[];
+y=[];
+for i=1:numel(m)
+  if (m(i)<0.005 == 1)
+    x=[x,m(i)];
+    y=[y,i];
+  endif
+end
+j=ceil(y./Population);
+[a,b]=min(j);
+MinTotalAnsweredSurvey=a
